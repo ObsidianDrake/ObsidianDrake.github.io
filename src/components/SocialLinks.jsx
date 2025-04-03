a5512aff7dd663048a97afa1460996b521c92456
Obsidian <shadowdrake02@gmail.com>
2025-04-04 02:03:12 +0800

    reference img must be import

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -2,14 +2,19 @@ import React from "react";
 import "/src/styles/SocialLinks.css";
 import SectionHead from '/src/components/SectionHead';
 import { useInView } from "react-intersection-observer";
+import facebook_img from '/src/assets/images/facebook.png';
+import twitter_img from '/src/assets/images/twitter-x.png';
+import discord_img from '/src/assets/images/discord.png';
+import bluesky_img from '/src/assets/images/bluesky.png';
+import github_img from '/src/assets/images/github.png';
 
 const SocialLinks = () => {
   const medias = [
-    ["facebook.png", "https://www.facebook.com/obsidiandrakee"],
-    ["twitter-x.png", "https://x.com/obsidiandrakee"],
-    ["discord.png", "https://discordapp.com/users/622288266951131137"],
-    ["bluesky.png", "https://bsky.app/profile/obsidiandrake.bsky.social"],
-    ["github.png", "https://github.com/ObsidianDrake"],
+    [facebook_img, "https://www.facebook.com/obsidiandrakee"],
+    [twitter_img, "https://x.com/obsidiandrakee"],
+    [discord_img, "https://discordapp.com/users/622288266951131137"],
+    [bluesky_img, "https://bsky.app/profile/obsidiandrake.bsky.social"],
+    [github_img, "https://github.com/ObsidianDrake"],
   ];
 
   const { ref, inView } = useInView({
@@ -29,7 +34,7 @@ const SocialLinks = () => {
               <a href={media[1]} target="_blank" rel="noopener noreferrer">
                 <img
                   className="media-icon"
-                  src={`/src/assets/images/${media[0]}`}
+                  src={`${media[0]}`}
                   alt={media[0].split(".")[0]}
                 />
               </a>
