0a548d4be4e76751606bf8ae3353ea7255ac3989
Obsidian <shadowdrake02@gmail.com>
2026-03-23 01:13:57 +0800

    feat: prevent right-click context menu and dragging for images

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,19 @@ import App from "./App";
 import "./styles/global.css";
 import { LanguageProvider } from "./i18n/LanguageContext";
 
+// 禁止所有圖片的右鍵菜單與拖曳
+document.addEventListener("contextmenu", (e) => {
+  if (e.target && e.target.tagName === "IMG") {
+    e.preventDefault();
+  }
+});
+
+document.addEventListener("dragstart", (e) => {
+  if (e.target && e.target.tagName === "IMG") {
+    e.preventDefault();
+  }
+});
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <LanguageProvider>
